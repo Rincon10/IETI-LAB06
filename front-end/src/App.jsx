13d110a168203b7effa8b91e7d25d069b5c93168
Camilo Rincon <ivan.rincon-s@mail.escuelaing.edu.co>
2022-03-20 15:22:54 -0500

    Husky pre-commit working

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,28 +1,26 @@
-import React, { useEffect, useReducer } from 'react';
-import { UserContext } from 'context/UserContext';
-import authReducer from 'components/auth/authReducer';
-import AppRouter from 'components/routers/AppRouter';
+import React, { useEffect, useReducer } from "react";
+import { UserContext } from "context/UserContext";
+import authReducer from "components/auth/authReducer";
+import AppRouter from "components/routers/AppRouter";
 
 const App = () => {
-    const aa = 0;
-    const init = () => {
-        return JSON.parse(localStorage.getItem('user')) || { logged: false };
-    };
+  const init = () => {
+    return JSON.parse(localStorage.getItem("user")) || { logged: false };
+  };
 
-    /* const [state, dispatch] = useReducer(reducer, initialState, init) */
-    const [user, dispatch] = useReducer(authReducer, {}, init);
+  /* const [state, dispatch] = useReducer(reducer, initialState, init) */
+  const [user, dispatch] = useReducer(authReducer, {}, init);
+  useEffect(() => {
+    localStorage.setItem("user", JSON.stringify(user));
+  }, [user]);
 
-    useEffect(() => {
-        localStorage.setItem('user', JSON.stringify(user));
-    }, [user]);
-
-    return (
-        <main id="main-container">
-            <UserContext.Provider value={{ user, dispatch }}>
-                <AppRouter />
-            </UserContext.Provider>
-        </main>
-    );
+  return (
+    <main id="main-container">
+      <UserContext.Provider value={{ user, dispatch }}>
+        <AppRouter />
+      </UserContext.Provider>
+    </main>
+  );
 };
 
 export default App;
